a4bf83b167b00cb3f01e1bd106a28e6914a53866
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-08-16 17:02:54 +0100

    Added binary search v1

--- Algorithms/binarySearch.js
+++ Algorithms/binarySearch.js
@@ -4,7 +4,7 @@
 
 const arr = ['a','b','c','e','h','z'];
 
-// Less efficient method - O(n)
+// Less search efficient method - O(n)
 
 function search (arr, target) {
     for (let i = 0; i < arr.length; i++) {
@@ -12,5 +12,37 @@ function search (arr, target) {
     }
 }
 
-console.log("Target index value: ", search(arr, 'e'))
+// Test Case:
+// console.log("Target index value: ", search(arr, 'e'))
 
+// Binary Search - O(logn)
+
+const arr2 = ['a','b','c','d','e','f','g']
+
+function binarySearch(arr, target, start=0, end=arr.length-1){
+
+    console.log('Start:', start, 'End: ', end)
+
+    if (start > end) {
+        console.log('Not found!');
+        return -1;
+    }
+
+    const middle = Math.floor( start + end / 2);
+    console.log('Middle: ', middle)
+
+    if (arr[middle] === target) {
+        console.log('Target Index: ', middle);         // This console log never fires when selecting the last position index in the array 
+        return middle;
+    }
+
+    if (arr[middle] > target) {
+        binarySearch(arr,target,start,middle-1);
+    }
+
+    if (arr[middle] < target) {
+        binarySearch(arr,target,middle+1,end);
+    }
+}
+
+binarySearch(arr2,'g')
