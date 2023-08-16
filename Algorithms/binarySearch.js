a9c1911c9585cf0dc592f050c2c120aa380a17a0
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-08-16 16:35:26 +0100

    Added problem statement

---
+++ Algorithms/binarySearch.js
@@ -0,0 +1,9 @@
+// Given an array of sorted elements and a target value, 
+// Return the index of the target value in the array.
+// If the target value is not in the array, return -1.
+
+const arr = ['a','b','c','e','h','z'];
+
+// Less efficient method - O(n)
+
+
