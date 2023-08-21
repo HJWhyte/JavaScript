5432d0404222939c6d840d4022987adc0efe1f28
Harry Whyte~ <harrywhyte@kubrickgroup.com>
2023-08-21 15:31:40 +0100

    Created Express app foundation

--- Projects/dream/server.js
+++ Projects/dream/server.js
@@ -7,4 +7,26 @@ const configuration = new Configuration ({
     apiKey: process.env.OPENAI,                       // Authorize via API Key
 }); 
 
-const openai = new OpenAIApi(configuration);   // Initialize OpenAI API SDK            
+const openai = new OpenAIApi(configuration);   // Initialize OpenAI API SDK
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();                    // Create express app
+app.use(cors());                        // Cross-Origin Resource Sharing - Middleware (Research!)
+app.use(express.json());              // App accepts JSON format
+
+app.post("/dream", async (req, res) => {
+
+    const prompt = req.body.prompt;   // Read request body prompt 
+
+    const aiResponse = await openai.createImage({         // Create 1 image based off the prompt of selected size
+        prompt,                  
+        n: 1,
+        size: '1024x1024',
+    });
+
+    const image = aiResponse.data.data[0].url;  // Image URL response object
+    res.send({ image });  // Send image in response 
+})
+
